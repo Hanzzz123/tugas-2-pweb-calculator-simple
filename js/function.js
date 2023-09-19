cafada880cd17f5fd54930b6c69bb9391e39b10a
Hanzzz123 <aanzs496@gmail.com>
2023-09-19 22:14:14 +0700

    Tugas Pweb 10/09/2023 membuat kalkulator simple menggunakan js dan html

---
+++ js/function.js
@@ -0,0 +1,29 @@
+function add(){
+    let x = parseFloat(document.getElementById("Num1").value)
+    let y = parseFloat(document.getElementById("Num2").value)
+    let result = x + y
+    document.getElementById("result").innerHTML = "result: " + result;
+}
+
+
+function substract(){
+    let x = parseFloat(document.getElementById("Num1").value)
+    let y = parseFloat(document.getElementById("Num2").value)
+    let result = x - y
+    document.getElementById("result").innerHTML = "result: " + result;
+}
+
+function multiply(){
+    let x = parseFloat(document.getElementById("Num1").value)
+    let y = parseFloat(document.getElementById("Num2").value)
+    let result = x * y
+    document.getElementById("result").innerHTML = "result: " + result;
+}
+
+
+function divide(){
+    let x = parseFloat(document.getElementById("Num1").value)
+    let y = parseFloat(document.getElementById("Num2").value)
+    let result = x + y
+    document.getElementById("result").innerHTML = "result: " + result;
+}
